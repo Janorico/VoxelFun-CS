4e838c9b8c45d28642ad7e535d639c00aa21a41c
Janosch Lion <janorico@posteo.de>
2023-05-03 18:18:54 +0200

    Do some optimizations in Crosshair.gd

--- Crosshair.gd
+++ Crosshair.gd
@@ -1,6 +1,9 @@
 extends Sprite
 
+
 var current_viewport_size = Vector2()
+
+
 func _ready():
 	var x = get_viewport().get_visible_rect().size.x / 2.0
 	var y = get_viewport().get_visible_rect().size.y / 2.0
@@ -8,6 +11,7 @@ func _ready():
 	
 	self.position = Vector2(x, y)
 	get_tree().get_root().connect("size_changed", self, "_on_size_changed")
+
 
 func _on_size_changed():
 	var x = get_viewport().get_visible_rect().size.x / 2.0
@@ -19,7 +23,3 @@ func _on_size_changed():
 	
 	self.scale = Vector2(self.scale.x * size_ratio.x, self.scale.y * size_ratio.y)
 	self.position = Vector2(x, y)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
