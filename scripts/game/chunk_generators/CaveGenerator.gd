d9c4818f4697f80f0e3d49e40c772ac4101b73ce
Janosch Lion <janorico@posteo.de>
2023-05-25 15:33:15 +0200

    Add cave generator

---
+++ scripts/game/chunk_generators/CaveGenerator.gd
@@ -0,0 +1,16 @@
+extends ChunkGenerator
+
+
+static func generate_surface(height, _x, y, _z):
+	var type
+	if y == height - 1:
+		type = "Grass"
+	elif y == height -2:
+		type = "Dirt"
+	elif y == height - 3 or y == height - 4:
+		type = "Stone"
+	elif y < height - 14:
+		type = "Stone"
+	else:
+		type = "Air"
+	return type
